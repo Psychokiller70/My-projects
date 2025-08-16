7a9c3fd42499f9154603ddd4c3f3c25be3f58b5e
Vansh <125341678+Psychokiller70@users.noreply.github.com>
2025-08-16 20:29:07 +0530

    Create 1-program.js

---
+++ JS practice code /1-program.js
@@ -0,0 +1,33 @@
+// let name = "vansh";
+// const age = 20;
+// var city = "delhi";
+
+// console.log(name + age + city);
+
+// // alert("vansh");
+// console.log("vansh");
+
+// function greet() {
+//     alert("Good night");
+// }
+
+// let a = "vansh";
+// let b = a + 45;
+// console.log(b);
+// console.log(typeof b);
+
+// const obj = {
+//     name : "vansh",
+//     age : 18
+// };
+
+// console.log(obj);
+
+// obj.city = "delhi";
+// console.log(obj);
+
+console.log("hello");
+
+for(i=1; i<=20; i++){
+    console.log("hello " + i)
+}
