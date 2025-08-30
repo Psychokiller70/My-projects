6a294e484356b65d8c0bf5aa462339d383b64997
Vansh <125341678+Psychokiller70@users.noreply.github.com>
2025-08-30 12:55:04 +0530

    Create DOM.js

---
+++ JS practice code /DOM.js
@@ -0,0 +1,28 @@
+document.querySelector(".box")
+<div class="box">
+
+document.querySelector(".box").innerHTML
+"hey i am a box"
+
+document.querySelector(".container").innerHTML
+Uncaught TypeError: document.querySelector(...) is null
+    <anonymous> debugger eval code:1
+debugger eval code:1:10
+document.querySelector(".conatiner").innerHTML
+'
+		<div class="box">hey i am a box</div>
+	'
+document.querySelector(".conatiner").OuterHTML
+undefined
+document.querySelector(".conatiner").outerHTML
+
+'<div class="conatiner">
+		<div class="box">hey i am a box</div>
+	</div>'
+
+document.querySelector(".conatiner").tagnode
+undefined
+document.querySelector(".conatiner").tagName
+"DIV"
+document.querySelector(".conatiner").nodeName
+"DIV" 
