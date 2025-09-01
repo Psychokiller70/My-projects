1777fc08d7a633afc9675b72fbe5f8fe4d13966b
Vansh <125341678+Psychokiller70@users.noreply.github.com>
2025-09-01 19:14:47 +0530

    Update DOM.js

--- JS practice code /DOM.js
+++ JS practice code /DOM.js
@@ -49,9 +49,12 @@ document.querySelector(".conatiner").getAttribute("style")
 document.querySelector(".conatiner").setAttribute("style", "background-color: blue")
 undefined 
 
-
-
-document.
+ // let div = document.createElement("div");
+            // div.innerHTML = "I have been inserted <b>by harry</b>"
+            // div.setAttribute("class", "created");
+            // document.querySelector(".container").before(div);
+            let cont = document.querySelector(".container")
+            cont.insertAdjacentHTML("beforebegin", "<b> I am under the water. Please h elp me here too much raining.... iuuuuooooo</b>")
 
 
 
